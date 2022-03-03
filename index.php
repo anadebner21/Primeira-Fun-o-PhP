24cfab37ed413df1f00a1248e7ad0c0b54bb1e23
anadebner <60924204+anadebner21@users.noreply.github.com>
2022-03-03 15:56:11 -0300

    calcula área do triangulo

--- index.php
+++ index.php
@@ -2,15 +2,25 @@
 
 $entradaPeso = (float) 81.5; 
 $entradaAltura = (float) 1.75;
-
+$entradaTriaAltura = (int) 4;
+$entradaTriaBase = (int) 6;
 
 function calculaIMC(float $entradaPeso, float $entradaAltura):float 
 {
     $imc = $entradaPeso/($entradaAltura^2);
     return $imc; 
+  
 }
-
 $saidaIMC = calculaIMC($entradaPeso, $entradaAltura); 
 echo "O seu Imc é: ".$saidaIMC;
+
+function calculaArea(int $entradaTriaBase, int $entradaTriaAltura):int 
+{
+    $area = ($entradaTriaBase * $entradaTriaAltura)/ 2;
+    return $area; 
+}
+
+$saidaArea = calculaArea($entradaTriaBase, $entradaTriaAltura);
+echo "A área total do triângulo é: ".$saidaArea; 
 
 ?>
